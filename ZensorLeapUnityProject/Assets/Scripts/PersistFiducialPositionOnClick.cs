f530d5b14f3d22602919a2a624cabf2d723f71c4
Paul Sorenson <psoren@umich.edu>
2019-01-31 16:17:27 -0500

    Image tracking updates

---
+++ ZensorLeapUnityProject/Assets/Scripts/PersistFiducialPositionOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class PersistFiducialPositionOnClick : MonoBehaviour {
+
+    public GameObject _FiducialCube;
+    public GameObject _PersistedOrigin;
+
+    void Awake()
+    {
+        MLInput.Start();
+        MLInput.OnControllerButtonDown += OnButtonDown;
+    }
+
+    void OnButtonDown(byte controller_id, MLInputControllerButton button)
+    {
+        _PersistedOrigin.transform.position = _FiducialCube.transform.position;
+        _PersistedOrigin.transform.rotation = _FiducialCube.transform.rotation;
+
+    }
+}
