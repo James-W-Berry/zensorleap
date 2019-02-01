579ec333b01dca0901b6d7ec570f8898241503fd
Austin Zimmer <austin.zimmer@zenuity.com>
2019-01-31 19:02:52 -0500

    add typed accessors to MountedSensor

--- ZensorLeapUnityProject/Assets/Scripts/DataModels/MountedSensor.cs
+++ ZensorLeapUnityProject/Assets/Scripts/DataModels/MountedSensor.cs
@@ -1,8 +1,18 @@
-﻿[System.Serializable]
+﻿using UnityEngine;
+
+[System.Serializable]
 public class MountedSensor {
 
 	public string type;
 	public double[] mountPoint;
 	public double[] orientation;
+
+	public Quaternion getOrientationAsQuaterian() {
+		return new Quaternion((float)orientation[0], (float)orientation[1], (float)orientation[2], (float)orientation[3]);
+	}
+
+	public Vector3 getMountPointAsVector3() {
+		return new Vector3((float)mountPoint[0], (float)mountPoint[1], (float)mountPoint[2]);
+	}
 
 }
